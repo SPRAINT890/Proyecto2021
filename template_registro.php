45177b1ecc2a822169f1c87c20e9a0985b4504fe
Eduardo <edpe2003@gmail.com>
2021-06-07 19:48:45 -0300

    Cambio

---
+++ template_registro.php
@@ -0,0 +1,24 @@
+<label for="Name">Nombre</label>
+<input name="Name" type="text" require>
+
+<label for="SecondName">Segundo Nombre</label>
+<input name="SecondName" type="text">
+
+<label for="LastName">Apellido</label>
+<input name="LastName" type="text" require>
+
+<label for="ScndLastName">Segundo Apellido</label>
+<input name="ScndLastName" type="text">
+
+<label for="Ci">Cedula</label>
+<input name="Ci" type="number" placeholder="53636064" require>
+
+<label for="Email">Correo</label>
+<input name="Email" type="email" require>
+
+<label for="Passwd">Contraseña</label>
+<input name="Passwd" type="password" require>
+
+<label for="VerfPasswd">Confirmar Contraseña</label>
+<input name="VerfPasswd" type="button" require>
+aaa
