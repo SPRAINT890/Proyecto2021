327f3a69bfdf2fd16184a6d8b8985203db1f4f53
Eduardo <edpe2003@gmail.com>
2021-06-07 19:15:07 -0300

    Listo el template_login

--- template_login.php
+++ template_login.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-    <body>
-        <label for="mail">Correo: </label>
-        <input name="mail" id="mail" type="email" required>
-        <label for="password">Contraseña: </label>
-        <input name="password" id="password" type="password" required>
-    </body>
-</html>
+<label for="mail">Correo: </label>
+<input name="mail" id="mail" type="email" required>
+<label for="password">Contraseña: </label>
+<input name="password" id="password" type="password" required>
