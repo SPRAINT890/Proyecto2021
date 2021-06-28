dc854a5b71cf180fd929a9a5cf8be43016b9363a
Eduardo <edpe2003@gmail.com>
2021-06-28 12:47:58 -0300

    .

--- templateNav.php
+++ templateNav.php
@@ -1,6 +1,9 @@
+<?php
+include('const.php');
+?>
 <nav>
     <ul>
-        <li><a href="">Consulta</a></li>  
-        <li><a href="">Cuenta</a></li>
+        <li><a href='<?php DIR ?>/Consulta.php'>Consulta</a></li>  
+        <li><a href='<?php DIR ?>/Cuenta.php'>Cuenta</a></li>
     </ul>
 </nav>
